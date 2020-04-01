26a32d82da59667b3ce7e1f71d51a3d733b5c2b5
gabbydgab <codingmattersio@gmail.com>
2020-04-01 14:56:50 +0800

    initial index page

--- modules/Site/Resources/views/index.blade.php
+++ modules/Site/Resources/views/index.blade.php
@@ -1,26 +1,22 @@
 @extends('site::layouts.master')
 
+@push('css')
+<style type="text/css">
+    #value-proposition {
+        background-image: url("/images/bg-workstation.jpg");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+</style>
+@endpush
+
 @section('content')
-<section class="hero is-fullheight" id="home">
-    <div class="hero-head">
-        <nav class="navbar is-transparent" role="navigation">
-            <div class="container">
-                <div class="navbar-brand">
-                    <a class="navbar-item" href="{{route('site.index')}}">
-{{--                        <h2 class="has-text-weight-bold is-size-1">Coding Matters</h2>--}}
-                    </a>
-                </div>
-                <div id="navbarMenuHeroA" class="navbar-menu">
-                    <div class="navbar-end">
-{{--                        <a class="navbar-item">Home</a>--}}
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </div>
+<section class="hero is-fullheight is-fullwidth" id="value-proposition">
     <div class="hero-body">
         <div class="container is-fluid has-text-centered">
-            <p class="is-size-4">Sorry for inconvenience. Site under construction.</p>
+            <h1 class="is-size-1 has-text-weight-bold has-text-white">Just don’t make it work, build it right!</h1>
+            <p class="is-size-5 has-text-grey-lighter">Improving code quality: readability and cognitive in structure</p>
         </div>
     </div>
 </section>
